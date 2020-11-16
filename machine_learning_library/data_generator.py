774a53a6bcb26d0c82361b7c85b7b49204746769
Shengwei <mun.wsw@gmail.com>
2020-11-17 00:03:37 +0800

    init

---
+++ machine_learning_library/data_generator.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+
+class DGP:
+    def __init__(self, observations, dimensions, random_state=1024):
+        self._observations = observations
+        self._dimensions = dimensions
+        self._random_state = random_state
+        self._features = None
+        self._labels = None
+        self._train_x = None
+        self._test_x = None
+        self._train_y = None
+        self._test_y = None
+
+    def generate(self, usage='regression'):
+        features, labels = self._generate()
+
+        self._features = features
+        self._usage = usage
+        self._labels = self._get_labels(labels)
+
+        if self.usage == 'regression':
+            return labels
+
+        if self.usage == 'binaryclassification':
+            return np.array([0 if x < 0 else 1 for x in labels]).reshape(-1, 1)
+
+        if self.usage == 'multiclassification':
+            return np.array([0 if x < -1 else
+                             1 if x < 1 else
+                             2 for x in labels]).reshape(-1, 1)
+
+    def _generate(self):
+        np.random.seed(self._random_state)
+        x = np.random.normal(size=(self._observations, self._dimensions))
+        error = np.random.normal(size=(self._observations, 1))
+        b = np.random.uniform(size=(self._dimensions, 1))
+        y = x @ b + error
+
+        return x, y
+
+    def split(self, rate=0.7, scale=False):
+
+        if scale:
+            self._features = (self._features - np.mean(self._features)) / self._features.std()
+
+        index = list(range(self._observations))
+        np.random.shuffle(index)
+
+        train = index[:int(rate * self._observations)]
+        test = index[int(rate * self._observations):]
+
+        self._train_x = self._features[train]
+        self._test_x = self._features[test]
+        self._train_y = self._labels[train]
+        self._test_y = self._labels[test]
+
+        return self._train_x, self._test_x, self._train_y, self._test_y
+
+    @property
+    def features(self):
+        return self._features
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
+    def usage(self):
+        return self._usage
---
+++ machine_learning_library/model_linear_regression.py
@@ -0,0 +1,82 @@
+from numpy import linalg
+import numpy as np
+
+
+class Regression:
+    def __init__(self, intercept=None):
+        self.beta = None
+        self.intercept = intercept
+
+    def fit(self, features, labels):
+        features = self._add_bias(features)
+
+        self._fit(features, labels)
+
+    def predict(self, features):
+        features = self._add_bias(features)
+        return self._predict(features)
+
+    def _add_bias(self, features):
+        if self.intercept:
+            ones = np.ones((len(features), 1))
+            return np.hstack([ones, features])
+
+        return features
+
+    def _fit(self, features, labels):
+        raise NotImplementedError
+
+    def _predict(self, features):
+        return features @ self.beta
+
+
+class LeastSquare(Regression):
+    def __init__(self, intercept=False):
+        super().__init__(intercept)
+
+    def _fit(self, features, labels):
+        xx = features.T @ features
+        xy = features.T @ labels
+
+        self.beta = linalg.solve(xx, xy)
+
+
+class Ridge(Regression):
+    def __init__(self, intercept=False, _lambda=0.2):
+        super().__init__(intercept)
+
+        self._lambda = _lambda
+
+    def _fit(self, features, labels):
+        identity = self._lambda * np.identity(features.shape[1])
+        xy = features.T @ labels
+        xx = features.T @ features
+        self.beta = np.linalg.inv(xx + identity) @ xy
+
+
+class GradientDescent(Regression):
+    def __init__(self, intercept=False, learning_rate=0.01, batch=10000,
+                 threshold=1e-5, random=False):
+        super().__init__(intercept)
+
+        self._learning_rate = learning_rate
+        self._batch = batch
+        self._threshold = threshold
+        self._random = random
+
+    def _fit(self, features, labels):
+        observations, dimensions = features.shape
+
+        beta = np.random.randn(dimensions).reshape(-1, 1) if self._random \
+            else np.ones(dimensions).reshape(-1, 1)
+
+        for i in range(self._batch):
+
+            error = features @ beta - labels
+            update = features.T @ error * (self._learning_rate / observations)
+            beta -= update
+
+            if np.abs(update).sum() < self._threshold:
+                self.beta = beta
+                return
+        raise ValueError('Gradient did not converge')
