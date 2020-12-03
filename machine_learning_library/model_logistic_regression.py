046d4173046cad0a719f6b9805dd63a13f7914b5
Shengwei Wu <mun.wsw@gmail.com>
2020-12-03 22:53:42 +0800

    add  gradient descent for logistic regression

--- machine_learning_library/model_logistic_regression.py
+++ machine_learning_library/model_logistic_regression.py
@@ -1,39 +1,43 @@
-from machine_learning_library.model_discriminant_analysis import  Classification
+from machine_learning_library.model_discriminant_analysis import Classification
 import numpy as np
 
 
 class LogsitcRegression(Classification):
-    def __init__(self, learning_rate=0.01, threshold=1e-5, epochs=10000):
+    def __init__(self, learning_rate=0.01, threshold=1e-5, epochs=10000, method='MLE'):
         super(LogsitcRegression, self).__init__()
         self.learning_rate = learning_rate
         self.threshold = threshold
         self.epochs = epochs
-        self._beta= None
-
+        self._beta = None
+        self.method = method
 
     def _fit(self, features, labels):
         features = self._add_bias(features)
         beta = np.ones((features.shape[1], 1))
 
         for _ in range(self.epochs):
-            x = features@beta
+            x = features @ beta
             p = self._sigmoid(x)
-            jaco = self._jacobia(features, labels, p)
-            hess = self._hession(features, p)
-
-            update = np.linalg.inv(hess)@jaco
+            update = self._mle(features, labels, p) if self.method == 'MLE' else \
+                self.learning_rate * self._jacobia(features, labels, p)
 
             if self._check_converge(update):
                 self._beta = beta
                 return
             beta += update
 
-        self._beta=beta
+        self._beta = beta
+
+    def _mle(self, features, labels, p):
+        jaco = self._jacobia(features, labels, p)
+        hess = self._hession(features, p)
+        update = np.linalg.inv(hess) @ jaco
+        return update
 
     def _predict(self, features):
         features = self._add_bias(features)
-        prob =  self._sigmoid(features @ self._beta)
-        return np.where(prob>0.5, 1, 0)
+        prob = self._sigmoid(features @ self._beta)
+        return np.where(prob > 0.5, 1, 0)
 
     def _check_converge(self, value):
         if np.sum(np.abs(value)) <= self.threshold:
@@ -48,7 +52,7 @@ class LogsitcRegression(Classification):
 
     @staticmethod
     def _jacobia(feature, label, p):
-        return feature.T@(label - p)
+        return feature.T @ (label - p)
 
     @staticmethod
     def _hession(feature, sigmoid):
@@ -58,4 +62,4 @@ class LogsitcRegression(Classification):
 
     @staticmethod
     def _sigmoid(x):
-        return 1/(1 + np.exp(-x))
+        return 1 / (1 + np.exp(-x))
--- test_machine_learning/test_logistic_regression.py
+++ test_machine_learning/test_logistic_regression.py
@@ -3,13 +3,28 @@ from wu_machine_learning.machine_learning_library.data_generator import DGP
 from machine_learning_library.model_evaluation import confusion_matirx
 
 
-def test_one_start():
+def test_mle_one_start():
     observations, dimensions = 1000, 5
     data = DGP(observations, dimensions)
     data.generate(usage='binaryclassification')
     train_x, test_x, train_y, test_y = data.split()
 
     model = LogsitcRegression()
+
+    model.fit(train_x, train_y)
+
+    pred = model.predict(test_x)
+
+    assert confusion_matirx(test_y, pred).values.tolist() == [[103, 51],
+                                                              [35, 111]]
+
+def test_gd_one_start():
+    observations, dimensions = 1000, 5
+    data = DGP(observations, dimensions)
+    data.generate(usage='binaryclassification')
+    train_x, test_x, train_y, test_y = data.split()
+
+    model = LogsitcRegression(method='GD')
 
     model.fit(train_x, train_y)
 
