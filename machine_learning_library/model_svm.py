6174f0c56a7399771b7fd71e4278af9b03d1d702
Shengwei Wu <mun.wsw@gmail.com>
2020-12-06 22:25:19 +0800

    add kernel function for svm

--- machine_learning_library/model_svm.py
+++ machine_learning_library/model_svm.py
@@ -3,17 +3,20 @@ import numpy as np
 
 
 class SVM(Classification):
-    def __init__(self, C=0.1, learning_rate=0.01, epochs=1000, threshold=0.0001):
+    def __init__(self, C=0.1, learning_rate=0.01, epochs=1000, threshold=0.0001, kernel=None, polynominal=2):
         super(SVM, self).__init__()
 
         self.C = C
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.threshold = threshold
+        self.kernel = kernel
+        self._polynominal = polynominal
 
     def _fit(self, features, labels):
 
         features = self._add_bias(features)
+        features = self._get_kernel(features)
         beta = np.zeros((features.shape[1], 1))
 
         labels = self._sign(labels)
@@ -31,6 +34,7 @@ class SVM(Classification):
 
     def _predict(self, features):
         features = self._add_bias(features)
+        features = self._get_kernel(features)
 
         return (features @ self._beta > 0).astype(int)
 
@@ -43,3 +47,21 @@ class SVM(Classification):
     @staticmethod
     def _sign(labels):
         return np.where(labels > 0, 1, -1)
+
+    def _get_kernel(self, x):
+        if self.kernel is None:
+            return x
+
+        z = x
+        if self.kernel == 'linear':
+            return x @ (z.T)
+
+        if self.kernel == 'polynominal':
+            return np.power(1 + x @ z.T, self._polynominal)
+
+        if self.kernel == 'rbf':
+            ## use euclidean_dist_matrix K(x, y) = e^(-g||x - z||^2)
+            norms_1 = (x ** 2).sum(axis=1)
+            norms_2 = (z ** 2).sum(axis=1)
+            distance = np.abs(norms_1.reshape(-1, 1) + norms_2 - 2 * np.dot(x, z.T))
+            return np.exp(-self._polynominal * distance)
